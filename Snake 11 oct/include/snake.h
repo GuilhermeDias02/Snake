3f98beeaf3e3b48ced0c353abdf2dd5e2ac3535a
GuilhermeDias02 <101174166+GuilhermeDias02@users.noreply.github.com>
2022-06-20 09:37:19 +0200

    Add files via upload

---
+++ Snake 11 oct/include/snake.h
@@ -0,0 +1,41 @@
+#ifndef SNAKE_H_INCLUDED
+#define SNAKE_H_INCLUDED
+
+#include <string>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
+class Snake
+{
+    public:
+        Snake(sf::RenderWindow &window, int Size1, int Speed1);
+
+        void Move();
+        void Update(sf::Event &event);
+        void Draw();
+        void GameOver();
+        void Score();
+        int restartgame = 1;
+        bool startScreen = false;
+        int Scoreint = 0;
+
+
+    private:
+        sf::RenderWindow &mWindow;
+        sf::RectangleShape Food;
+        int Check_Move;
+        int TimeToUpdate;
+        int Size2;
+        int Speed2;
+        bool Left, Right, Up, Down;
+        float CurrentTime;
+        std::vector<sf::RectangleShape> mSnakes;
+
+        void CheckCollision();
+        void AddCase();
+        void FoodGenerator();
+};
+
+#endif // SNAKE_H_INCLUDED
